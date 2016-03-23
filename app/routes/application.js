65183311a2a3c8a7f53d5d92f4f851016b22d8f0
trebek1 <trebek08@gmail.com>
2016-03-23 01:59:30 -0700

    Added submission box to index

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	actions:{
 		search: function(){
-			console.log("here")
+			
+			 var controller = this.get('controller');
+			 var title = controller.get('title');
+			 console.log("This is title ", title)
+			 controller.set('title', '');
+
 		}	
 	}
 	
