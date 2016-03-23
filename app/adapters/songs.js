37b51e5d944a0ae2cf2462dd5a61710ff89d7c6a
trebek1 <trebek08@gmail.com>
2016-03-22 21:53:38 -0700

    Got the data to come in asyncronously

---
+++ app/adapters/songs.js
@@ -0,0 +1,23 @@
+// import DS from 'ember-data';
+
+// export default DS.RESTAdapter.extend({
+	
+// 	findAll: function(store,type,id,snapshot){
+		
+// 		return $.ajax({
+// 		url: 'https://itunes.apple.com/search?term=linkin+park',
+// 		dataType: 'jsonp',
+// 		success: function(response){
+// 			console.log("resp? ", response)
+// 			return response
+// 		}
+// 		}).then(function(data){
+// 			console.log("this is dfaataadgagafgaffg 	", data)
+
+// 			return{ data: data
+// 				}
+				
+// 		})
+		
+// 	}
+// });
